e19e013a3f967c9a6319f5466d26247de075dac8
saifhussain98 <105277308+saifhussain98@users.noreply.github.com>
2022-06-30 09:49:06 +0100

    Arrays
    Working with Arrays

---
+++ Arrays/src/ArraysRFun/GymExercises.java
@@ -0,0 +1,28 @@
+package ArraysRFun;
+
+public class GymExercises {
+
+	public static void main(String[] args) {
+		System.out.println("Hello! I am your virtual personal assistant.");
+		
+		String MuscleGroup[] = {"Chest", "Shoulders","Back", "Arms", "Legs"};
+		String Exercise [] = {"Bench Press", "Military Press", "Lat Pull Downs", "Curls and Pulldowns", "Squats"};
+		
+		System.out.println("These are the Muscle groups you can work out!");
+		System.out.println(MuscleGroup[0]);
+		System.out.println(MuscleGroup[1]);
+		System.out.println(MuscleGroup[2]);
+		System.out.println(MuscleGroup[3]);
+		System.out.println(MuscleGroup[4]);
+		
+		System.out.println("These are the exercises to work out each Muscle group!");
+		System.out.println(Exercise[0]);
+		System.out.println(Exercise[1]);
+		System.out.println(Exercise[2]);
+		System.out.println(Exercise[3]);
+		System.out.println(Exercise[4]);
+	}
+
+	}
+
+
